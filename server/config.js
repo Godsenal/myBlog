4b38206d190a321217837837a04fd02dab9ecb78
Godsenal <tmqps78@gmail.com>
2017-08-06 19:13:02 +0900

    config path fixed

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ export const nodeEnv = env.NODE_ENV || 'development';
 
 export default {
   port: env.PORT || 8080,
-  portHttps: env.PORT || 8443,
+  portHttps: 8443,
   host: env.HOST || 'localhost',
   domain: env.DOMAIN || 'localhost',
   get serverUrl() {
@@ -15,7 +15,7 @@ export default {
   //dbUrl : 'mongodb://localhost:27017/chatDB',
   dbUrl : localDB,
   jwtSecret: 'Godsenal!3737',
-  pathToCert: __dirname + '/ssl/server.crt',
-  pathToKey: __dirname + '/ssl/server.key',
+  pathToCert: __dirname + '/ssl/lth.com.crt',
+  pathToKey: __dirname + '/ssl/lth.com.key',
   //sessionSecret: 'Godsenal!3737',
 };
