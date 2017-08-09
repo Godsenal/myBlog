58f007c7497603390720294553824b57b3569f3e
Godsenal <tmqps78@gmail.com>
2017-08-09 09:59:16 +0900

    production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './client/src/index.js',
   output: {
     path: __dirname + '/public',
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [
       {
